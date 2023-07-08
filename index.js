245beb83d4ed4aabd356ebbd128f6e334891e77b
Gaurav Arora <gaurav.arora@turtlemint.com>
2023-07-08 13:30:04 +0530

    Fixed bugs in 3rd api

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const axios = require('axios'); // require axios
 const cors = require('cors');
 const app = express();
 const multer = require('multer');
+const FormData = require('form-data');
+const fs = require('fs');
+const path = require('path');
 const port = 3000;
 
 
@@ -51,37 +54,38 @@ app.get('/fetch-voices', async (req, res) => {
 // Multer configuration for handling file uploads
 const upload = multer();
 
-// Define route to handle adding voices
-app.post('/voices/add', upload.single('files'), async (req, res) => {
-  const name = req.body.name;
-  const description = req.body.description;
-  const labels = req.body.labels;
-  const file = req.file;
+app.post('/addVoice', async (req, res) => {
+  const formData = new FormData();
+
+  // append fields to the formData
+  formData.append('name', 'test');
+  formData.append('description', 'test123');
+  formData.append('labels', '');
+
+  // append the file to formData
+  const fileStream = fs.createReadStream('AudioSamples/4.mp3');
+  formData.append('files', fileStream);
 
   try {
-    const formData = new FormData();
-    formData.append('name', name);
-    formData.append('description', description);
-    formData.append('labels', labels);
-    formData.append('files', file.buffer, {
-      filename: file.originalname,
-      contentType: file.mimetype
-    });
+      const response = await axios({
+          method: 'post',
+          url: 'https://api.elevenlabs.io/v1/voices/add',
+          data: formData,
+          headers: { 
+              ...formData.getHeaders(),
+              'accept': 'application/json',
+              'xi-api-key': '0aa0dd7a5776e591c74e58be393f6b21' 
+          },
+      });
 
-    const response = await axios.post('https://api.elevenlabs.io/v1/voices/add', formData, {
-      headers: {
-        'Accept': 'application/json',
-        'xi-api-key': '0aa0dd7a5776e591c74e58be393f6b21',
-        ...formData.getHeaders()
-      }
-    });
-
-    res.json(response.data);
+      // Send response data back to the client
+      res.json(response.data);
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Error occurred while adding voice');
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred' });
   }
 });
+
   
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
