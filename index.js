41a86f3414975c3cb8c260c74ad134c614298839
Sumit Jadiya <sumit.jadiya@turtlemint.com>
2023-07-08 13:58:21 +0530

    add dummy

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ const multer = require('multer');
 const FormData = require('form-data');
 const fs = require('fs');
 const path = require('path');
-const port = 3000;
+const port = 4001;
 
 
 app.use(express.json()); 
 app.use(cors()); 
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/dummy',(req, res) => {
+  res.status(200).json({
+    success: true,
+    greeting: `Hello from API!`,
+  });
+});
 
 app.get('/fetch-voices', async (req, res) => {
     try {
