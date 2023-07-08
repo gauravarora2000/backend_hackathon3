4d18cb3b54a07258dfa9f0309ad21ec49fd5ee53
Gaurav Arora <gaurav.arora@turtlemint.com>
2023-07-08 13:16:03 +0530

    added new api

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const axios = require('axios'); // require axios
 const cors = require('cors');
 const app = express();
-const port = 5500;
+const multer = require('multer');
+const port = 3000;
+
 
 app.use(express.json()); 
 app.use(cors()); 
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/fetch-voices', async (req, res) => {
     try {
@@ -42,6 +45,43 @@ app.get('/fetch-voices', async (req, res) => {
       res.status(500).send('Error occurred while making the text-to-speech API call');
     }
   });
+
+  
+
+// Multer configuration for handling file uploads
+const upload = multer();
+
+// Define route to handle adding voices
+app.post('/voices/add', upload.single('files'), async (req, res) => {
+  const name = req.body.name;
+  const description = req.body.description;
+  const labels = req.body.labels;
+  const file = req.file;
+
+  try {
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('description', description);
+    formData.append('labels', labels);
+    formData.append('files', file.buffer, {
+      filename: file.originalname,
+      contentType: file.mimetype
+    });
+
+    const response = await axios.post('https://api.elevenlabs.io/v1/voices/add', formData, {
+      headers: {
+        'Accept': 'application/json',
+        'xi-api-key': '0aa0dd7a5776e591c74e58be393f6b21',
+        ...formData.getHeaders()
+      }
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error occurred while adding voice');
+  }
+});
   
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
